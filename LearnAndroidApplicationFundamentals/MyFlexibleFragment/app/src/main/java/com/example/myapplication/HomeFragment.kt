cf5daee4e30d2c3f3b3745bb14d95ec7cf390b1e
wmprawiro <wahyump62@gmail.com>
2023-04-05 09:45:27 +0700

    update Flexible Fragment di Satu Activity

--- LearnAndroidApplicationFundamentals/MyFlexibleFragment/app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ LearnAndroidApplicationFundamentals/MyFlexibleFragment/app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -19,10 +19,23 @@ class HomeFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory : Button = view.findViewById(R.id.btn_category)
+        val btnCategory: Button = view.findViewById(R.id.btn_category)
         btnCategory.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(
+                    R.id.frame_container,
+                    categoryFragment,
+                    CategoryFragment::class.java.simpleName
+                )
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
