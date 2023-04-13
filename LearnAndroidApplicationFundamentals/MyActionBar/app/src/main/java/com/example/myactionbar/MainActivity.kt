ad624235d77c5a0b5f9a66d232c0a861143e7e04
wmprawiro <wahyump62@gmail.com>
2023-04-13 14:38:01 +0700

    add searchview

--- LearnAndroidApplicationFundamentals/MyActionBar/app/src/main/java/com/example/myactionbar/MainActivity.kt
+++ LearnAndroidApplicationFundamentals/MyActionBar/app/src/main/java/com/example/myactionbar/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myactionbar
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.example.myactionbar.databinding.ActivityMainBinding
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
         return true
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
